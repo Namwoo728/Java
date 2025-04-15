8cb6c887b890559d4084acfc2fe65b9ba37dceda
treegus <cowtree28@gmail.com>
2025-04-15 20:22:04 +0900

    project setting
    프젝 세팅했습

---
+++ 20250415Java/src/AddService.java
@@ -0,0 +1,10 @@
+public class AddService {
+
+    public void print() {
+        System.out.println(add(1, 2));
+    }
+
+    private int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ 20250415Java/src/Car.java
@@ -0,0 +1,6 @@
+public class Car implements Vehicle {
+    @Override
+    public void run() {
+        System.out.println("Car is going");
+    }
+}
